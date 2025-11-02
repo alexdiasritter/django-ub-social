f4d15f55356ba14a004b668c67657a3e28fcf735
AlexRitter1 <ritter.alex@hotmail.com>
2025-11-02 10:26:58 -0300

    feat: urls api/v2

--- escola/urls.py
+++ escola/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
-# urls globais da escola
+from cursos.urls import router
 
 urlpatterns = [
-    path('api/v1/', include('cursos.urls')),
+    path('api/v2/', include(router.urls)),
     path('auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     
